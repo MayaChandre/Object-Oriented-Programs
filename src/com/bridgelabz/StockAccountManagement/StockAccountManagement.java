d3e2b62f12d66064ea10d781bf32ffb6e9f72448
mayavathi <mayachandre27@gmail.com>
2021-07-06 07:29:11 -0400

    Stock Account Management

---
+++ src/com/bridgelabz/StockAccountManagement/StockAccountManagement.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.StockAccountManagement;
+
+public class StockAccountManagement {
+
+		public void addStocks(Stocks share) {
+		}
+		public void getStockDetails() {
+		}
+		public void calculateShareValue() {
+		
+		}
+}
+	
+
---
+++ src/com/bridgelabz/StockAccountManagement/StockAccountManagementImpl.java
@@ -0,0 +1,39 @@
+package com.bridgelabz.StockAccountManagement;
+
+	import java.util.LinkedList;
+
+	public static void main(String[] args)
+	{
+		
+		StockAccountManagement stockservice = new StockAccountManagementImpl();
+		Scanner scanner = new Scanner(System.in);
+		{
+		System.out.println("welcome to Stock management");
+		{
+		System.out.println("Enter the num of shares to add: ");
+		int numOfShares = scanner.nextInt();
+		{
+		
+		for (int index = 0; index < numOfShares; index++) {
+			
+			Stocks newItem = new Stocks();
+			
+			System.out.println("Enter Share name: ");
+			newItem.setShareName(scanner.next());
+			System.out.println("Enter share Price: ");
+			newItem.setSharePrice(scanner.nextDouble());
+			System.out.println("Enter no. of share taken: ");
+			newItem.setShareCount(scanner.nextDouble());	
+			stockservice.addStocks(newItem);
+		}
+		stockservice.getStockDetails();
+		stockservice.calculateShareValue();
+
+		}
+		}
+		
+		}
+		}
+
+			
+
