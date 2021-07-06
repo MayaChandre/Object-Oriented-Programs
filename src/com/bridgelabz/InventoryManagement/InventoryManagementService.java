f2faac016b6070f2a5c6fea38b11a714cb5aa8da
mayavathi <mayachandre27@gmail.com>
2021-07-06 05:50:52 -0400

    Inventory Management

---
+++ src/com/bridgelabz/InventoryManagement/InventoryManagementService.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.InventoryManagement;
+
+public class InventoryManagementService {
+
+	public void addInventoryItem(Items item) {
+	}
+	public void getInventoryDetails() {
+	}
+	public void getInventoryDataByName(String itemName) {
+	}
+	public void calculateInventoryItemValue() {
+	}
+	
+
+}
---
+++ src/com/bridgelabz/InventoryManagement/InventoryManagementSevice.java
@@ -0,0 +1,5 @@
+package com.bridgelabz.InventoryManagement;
+
+public interface InventoryManagementSevice {
+
+}
